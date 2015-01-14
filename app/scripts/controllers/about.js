695e62dfae1110820106329f05c210d956a27496
Marco Marchese <marco.marchese75@gmail.com>
2015-01-14 16:35:55 +0100

    Versione 0.0.1
    Commit dopo modifiche per rendere funzionante il gruntfile.js e test su
    localhost:9000.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bedManFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bedManFeApp
+ */
+angular.module('bedManFeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bedManFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bedManFeApp
+ */
+angular.module('bedManFeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
